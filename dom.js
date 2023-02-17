1079da3bf80f4e052f60c7d2c6dbbabcf45d7ef6
diego1238 <125703749+diego1238@users.noreply.github.com>
2023-02-17 17:01:07 -0500

    dom y javescript
    Diego Andrés Gonzalez Castano utc grupo #5

---
+++ dom.js
@@ -0,0 +1,33 @@
+console.log("Entramos")
+
+var items = document.getElementsByClassName("items");
+
+var cantidad = items.length; 
+
+console.log(cantidad);
+var cantidad = items,length; 
+console.log("cantidad de lista")
+
+var div = document.createElement("div");
+
+
+ 
+
+var divUno = document.getElementById("uno");
+ 
+divUno.appendChild(div);
+
+
+
+
+var lista = document.getElementById("lista");
+
+var hijo = document.createElement("li");
+hijo.innerText = "li nuevo";
+lista.appendChild(hijo);
+
+document.getElementById("tres").style.color='red';
+document.getElementById("lista").style.color='green';
+
+
+
